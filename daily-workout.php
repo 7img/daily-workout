6affc088cba82554d73274280fafc69f9819c7a9
7imdev <tim@7im.nl>
2024-03-24 15:29:57 +0100

    Change author URL

--- daily-workout.php
+++ daily-workout.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.2
  * Requires PHP:      8.1
  * Author:            7imDev
- * Author URI:        https://wod-generator.com/wordpress-plugin/
+ * Author URI:        https://7im.nl
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       daily-workout
